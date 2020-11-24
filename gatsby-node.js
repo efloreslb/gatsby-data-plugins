bf3c926fb92f244a0457f659c0d551dc8caf29ce
Edgar F <eflores.ef@gmail.com>
2020-11-23 18:38:10 -0800

    Created blog post template generated from md files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -13,6 +14,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
  }
 
  exports.createPages = async ({ graphql, actions }) => {
+   const { createPage } = actions
     const result = await graphql(`
       query {
          allMarkdownRemark {
@@ -26,5 +28,15 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
          }
       }
     `)
-    console.log(JSON.stringify(result, null, 4))
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug,
+        },
+      })
+    })
  }
